ef549d74cf628523093148eab5d1e6f958d847d9
Bereza-M <bereznev@tut.by>
2022-04-26 22:02:43 +0300

    26.04

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
 import React from 'react';
 import './App.css';
-import ToDoList from "./ToDoList";
+import TodoList from "./TodoList";
 
 // CRUD - create, read, update, delete
-//GUI - графический интерфейс   CLI - command line interface
-function App() {
+// GUI - графический интерфейс   CLI - command line interface
+
+export type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+
+const App = () => {
+    const todoListTitle_1 = "What to learn"
+    const todoListTitle_2 = "What to buy"
+    const todoListTitle_3 = "What to read"
+    const tasks: Array<TaskType> = [
+        {id: 1, title: "HTML&CSS", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "React", isDone: false},
+    ]
+
     return (
         <div className="App">
-            <ToDoList />
-            <ToDoList />
-            <ToDoList />
-            <ToDoList />
+            <TodoList
+                title={todoListTitle_1}
+                tasks={tasks}
+            />
 
         </div>
     );
